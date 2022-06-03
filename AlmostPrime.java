c4192ff7b60294a59f0a9ef3a915f1bc0012d602
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-03 15:53:55 +0600

    Update AlmostPrime.java

--- AlmostPrime.java
+++ AlmostPrime.java
@@ -38,3 +38,45 @@ public static void imSeive(int n) {
          System.out.println(cnt);
 	}
 }
+
+// Second Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N=3010;
+bool ok[N];
+int Sieve[N];
+
+void precompute(){
+  for(int i=2;i<=N;i++){
+    if(!ok[i]){
+      for(int j=i;j<=N;j+=i){
+        Sieve[j]++;
+        ok[j]=true;
+      }
+    }
+  }
+}
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n,cnt=0;
+    cin >> n;
+    precompute();
+      for(int i=2 ;i<=n;i++){
+       if(Sieve[i]==2){
+         cnt++;
+       }
+  }
+  cout << cnt << '\n';
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
+
